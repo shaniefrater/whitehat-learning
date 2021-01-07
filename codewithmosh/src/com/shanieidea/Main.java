42653643a6ec4a65a26bee671307cff23df0922f
shaniefrater <shaniemarie@live.co.uk>
2021-01-07 17:03:37 +0000

    initialversion

---
+++ codewithmosh/src/com/shanieidea/Main.java
@@ -0,0 +1,9 @@
+package com.shanieidea;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double result = Math.round(Math.random() * 500);
+        System.out.println(result);
+    }
+}
