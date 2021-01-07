e8853c66da3d3582838f7dad5cee6a524b686cb9
shaniefrater <shaniemarie@live.co.uk>
2021-01-07 17:22:46 +0000

    Changed range from 500 to 100

--- codewithmosh/src/com/shanieidea/Main.java
+++ codewithmosh/src/com/shanieidea/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        double result = Math.round(Math.random() * 100);
+        double result = Math.round(Math.random() * 200);
         System.out.println(result);
     }
 }
